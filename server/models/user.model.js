089729bffae3b413a0eafc120fc45258724c2d6a
fxola <afolabiabass62@gmail.com>
2019-03-05 11:02:04 +0100

    [ft-164380678] Create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(id, email, firstName, lastName, password) {
+    this.id = id;
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.password = password;
+  }
+}
+
+export default User;
