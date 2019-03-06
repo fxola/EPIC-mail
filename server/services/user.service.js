9cbc9e720aa3b8a5059f85e1be6e995d127c01b9
fxola <afolabiabass62@gmail.com>
2019-03-06 14:39:30 +0100

    feat(user): implement password hashing and possible error handling

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mockData from '../utils/mockData';
 import User from '../models/user.model';
 
@@ -9,34 +10,43 @@ class UserService {
   static createUser(userDetails) {
     const { email, firstName, lastName, password } = userDetails;
 
-    // logic for creating a new user ID
-    const userLength = mockData.users.length;
-    const lastUserId = mockData.users[userLength - 1].id;
-    const id = lastUserId + 1;
+    try {
+      const hashedPassword = this.hashPassword(password);
 
-    const newUser = new User(id, email, firstName, lastName, password);
+      // logic for creating a new user ID
+      const userLength = mockData.users.length;
+      const lastUserId = mockData.users[userLength - 1].id;
+      const id = lastUserId + 1;
 
-    // updating the db with the newly created user
-    mockData.users.push(newUser);
+      const newUser = new User(id, email, firstName, lastName, hashedPassword);
 
-    const { payloadId, payloadEmail, payloadfirstName, payloadLastName } = newUser;
+      // updating the db with the newly created user
+      mockData.users.push(newUser);
 
-    const payload = {
-      payloadId,
-      payloadEmail,
-      payloadfirstName,
-      payloadLastName
-    };
+      const { payloadId, payloadEmail, payloadfirstName, payloadLastName } = newUser;
 
-    const bearerToken = this.getToken(payload);
-    return {
-      status: 201,
-      data: [
-        {
-          token: bearerToken
-        }
-      ]
-    };
+      const payload = {
+        payloadId,
+        payloadEmail,
+        payloadfirstName,
+        payloadLastName
+      };
+
+      const bearerToken = this.getToken(payload);
+      return {
+        status: 201,
+        data: [
+          {
+            token: bearerToken
+          }
+        ]
+      };
+    } catch (err) {
+      return {
+        status: 500,
+        error: err
+      };
+    }
   }
 
   static logUserIn(userCredentials) {
@@ -55,6 +65,18 @@ class UserService {
 
   static getToken(userPayload) {
     return jwt.sign(userPayload, process.env.SECRET, { expiresIn: '1h' });
+  }
+
+  static hashPassword(password) {
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) {
+        return {
+          status: 500,
+          error: err
+        };
+      }
+      return hash;
+    });
   }
 }
 
