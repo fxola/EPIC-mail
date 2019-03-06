33be2bf96f7af5e57a0849568aaa09b364a138fa
fxola <afolabiabass62@gmail.com>
2019-03-06 20:49:41 +0100

    feat(message): create message model

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -2,7 +2,7 @@ class Message {
   constructor(id, createdOn, subject, message, parentMessageId, status) {
     this.id = id;
     this.createdOn = createdOn;
-    this.firstName = subject;
+    this.subject = subject;
     this.message = message;
     this.parentMessageId = parentMessageId;
     this.status = status;
