ea0bd819b3deae8a28874c703fca84e039409152
fxola <afolabiabass62@gmail.com>
2019-03-07 16:00:50 +0100

    feat(message): Implement Auth middleware to get current user

--- server/middleware/Auth.js
+++ server/middleware/Auth.js
@@ -1,1 +1,22 @@
 import jwt from 'jsonwebtoken';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class Auth {
+  static getUser(req, res, next) {
+    try {
+      const decoded = jwt.verify(req.token, process.env.SECRET);
+      req.userEmail = decoded;
+      return next();
+    } catch (e) {
+      return res.status(401).send({
+        status: 401,
+        error: 'You do not have access to this page'
+      });
+    }
+  }
+}
+
+export default Auth;
